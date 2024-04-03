be67c52364c047867bca37de3a241823ce17fd6e
pedroxhenrique <spedrohenrique48@gmail.com>
2024-04-02 22:34:41 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a = 3
+b = 56
+c = 21
+d = 46
+resultado = a+b+c-d
+print('A soma de Abc é igual a',a+b+c,'subtraindo ',d,'resulta no valor',resultado)
+
+a1 = int(input('Digite um número:'))
+a2 = int(input('Digite outro número:'))
+valor = a1*a2
+print('O resultado de uma multiplicação com os dois números escolhidos e igual a',valor)
+
+
